33480a889da1724b1263559f8ce87c178f08273f
sebsa346 <sebsa346>
2013-12-16 18:03:25 +0100

    start up

---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Marble marbles = new Marble();
+            u1Label.DataContext = marbles;
+            u2Label.DataContext = marbles;
+            u3Label.DataContext = marbles;
+            u4Label.DataContext = marbles;
+            u5Label.DataContext = marbles;
+            u6Label.DataContext = marbles;
+
+            c1Label.DataContext = marbles;
+            c2Label.DataContext = marbles;
+            c3Label.DataContext = marbles;
+            c4Label.DataContext = marbles;
+            c5Label.DataContext = marbles;
+            c6Label.DataContext = marbles;
+
+            List<Marble> userMarbleList = new List<Marble>();
+            userMarbleList.Add(marbles);
+
+            List<Marble> compMarbleList = new List<Marble>();
+            compMarbleList.Add(marbles);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+       
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WpfApplication1/Marble.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace WpfApplication1
+{
+    public class Marble : INotifyPropertyChanged
+    {
+        private int noMarbles;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int Marbles
+        {
+            get { return noMarbles; }
+            set { noMarbles = value;
+            OnPropertyChanged("Marbles");
+            }
+        }
+
+        private void OnPropertyChanged(int noMarbles)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(noMarbles));
+            }
+            throw new NotImplementedException();
+        }
+
+    }
+}
